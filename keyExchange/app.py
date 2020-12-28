629121b483443c1c021d40e905d5c826e0aa3216
Catarina <c.alexandracorreia@ua.pt>
2020-12-28 16:55:01 +0000

    Add keyexchange server

---
+++ keyExchange/app.py
@@ -0,0 +1,39 @@
+import logging 
+
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+pubkeys = {}
+
+# explicar como a api funciona
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
+@app.route('/getkey/<idD>', methods=['GET'])
+def getKey(idD):
+    #idD = request.args.get('id')
+    if idD in pubkeys:
+        d = {'key': pubkeys[idD]}
+        return jsonify(d)
+    else:
+        logging.error('id in not in database')
+        return jsonify({})
+
+
+@app.route('/putkey', methods=['POST'])
+def putKey():
+    if request.is_json:
+        content = request.get_json()
+        idD = content['id']
+        pubkey = content['pubkey']
+        pubkeys[idD]=pubkey
+        return ('', 204)
+    else:
+        logging.error('This is an error message')
+
+
+
+if __name__ == '__main__':
+    app.run()
